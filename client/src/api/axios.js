f1828f95035ced9aa18d590233690b913d982bde
JaimeDGU <jaimeguzmanlt@gmail.com>
2024-07-22 14:05:05 -0400

    ahora si el ultmo

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosMOD = axios.create({
     withCredentials: true,
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api` || 'http://localhost:3001/api'
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}api` || 'http://localhost:3001/api'
 });
 
 export default axiosMOD;
